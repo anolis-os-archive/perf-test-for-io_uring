6d07a1a5302a51a19961b883cd67c1616db23db9
wangqiyilang <wangqiyilang@linux.alibaba.com>
2020-07-22 11:28:09 +0800

    meme fixes
    fix code format and change ap.doc from "" to nullptr.
    
    Signed-off-by: wangqiyilang <wangqiyilang@linux.alibaba.com>

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@ extern argp ap_loader;
 extern argp ap_test;
 
 static int merge_and_parse_arg(int argc, char **argv) {
-    argp_child apc[] = {{&ap_loader,0,"Loader Arguments"},
-                        {&ap_test,0,"Test Arguments"},
+    argp_child apc[] = {{&ap_loader, 0, "Loader Arguments"},
+                        {&ap_test,   0, "Test Arguments"},
                         {nullptr}};
 
-    argp ap = {nullptr, nullptr, nullptr, "", apc};
+    argp ap = {nullptr, nullptr, nullptr, nullptr, apc};
 
     return argp_parse(&ap, argc, argv, 0, nullptr, nullptr);
 }
